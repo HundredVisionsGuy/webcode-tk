1b95414ffa71bf58d8bfd2644a67ecdb8aa5c71a
Hundredvisionsguy <cwinikka@gmail.com>
2023-08-07 14:06:05 -0700

    adjusting ux_tools for better accuracy

--- tests/test_ux_tools.py
+++ tests/test_ux_tools.py
@@ -7,24 +7,68 @@ from webcode_tk import ux_tools
 
 
 @pytest.fixture
-def automate_text():
-    sample = clerk.file_to_string("tests/test_files/automate_boring_stuff.txt")
-    return sample
+def automate_path():
+    return "tests/test_files/automate_boring_stuff.html"
 
 
 @pytest.fixture
-def css_basics():
-    sample = clerk.file_to_string("tests/test_files/css_basics.txt")
-    return sample
+def automate_text():
+    text = clerk.file_to_string("tests/test_files/automate_paragraph_text.txt")
+    return text
 
 
-def test_get_flesch_kincaid_grade_level_for_12(automate_text):
-    results = ux_tools.get_flesch_kincaid_grade_level(automate_text)
+@pytest.fixture
+def css_basics_path():
+    return "tests/test_files/css_basics.html"
+
+
+@pytest.fixture
+def single_page_path():
+    return "tests/test_files/project/sports.html"
+
+
+@pytest.fixture
+def large_project_path():
+    return "tests/test_files/large_project/"
+
+
+def test_get_flesch_kincaid_grade_level_for_12(automate_path):
+    results = ux_tools.get_flesch_kincaid_grade_level(automate_path)
     expected = 12.1
     assert results == expected
 
 
-def test_get_flesch_kincaid_grade_level_for_4_5(css_basics):
-    results = ux_tools.get_flesch_kincaid_grade_level(css_basics)
-    expected = 4.9
+def test_get_flesch_kincaid_grade_level_for_5_2(css_basics_path):
+    results = ux_tools.get_flesch_kincaid_grade_level(css_basics_path)
+    expected = 5.2
+    assert results == expected
+
+
+def test_get_all_paragraphs_for_single_page(automate_path):
+    results = ux_tools.get_all_paragraphs(automate_path)
+    assert len(results) == 3
+
+
+def test_get_paragraph_text_for_automate_page(automate_path, automate_text):
+    paragraphs = ux_tools.get_all_paragraphs(automate_path)
+    results = ux_tools.get_paragraph_text(paragraphs)
+    assert results == automate_text
+
+
+def test_get_all_paragraphs_for_large_project(large_project_path):
+    results = ux_tools.get_all_paragraphs(large_project_path)
+    assert len(results) == 4
+
+
+def test_get_words_per_sentence_for_automate(automate_path):
+    results = ux_tools.get_words_per_paragraph(automate_path)
+    expected = 55.0
+    assert results == expected
+
+
+def test_simple_page_for_5_and_half_words_per_sentence(single_page_path):
+    """simple page has 2 paragraph tags and a header, 11 words in all.
+    That's 5.5 words per sentence"""
+    results = ux_tools.get_words_per_paragraph(single_page_path)
+    expected = 19.0
     assert results == expected
