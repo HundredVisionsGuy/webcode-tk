b7228d03d08be4b4cf05ba944892ef267d7001b8
Chris Winikka <3827026+HundredVisionsGuy@users.noreply.github.com>
2023-09-03 20:26:00 -0700

    updating version and docs

--- webcode_tk/__init__.py
+++ webcode_tk/__init__.py
@@ -9,7 +9,10 @@ Modules exported by this package:
 - `html`: gets html files from a project folder, gets the HTML code
           from files, gets number of a particular element in a file
           or folder, gets elements as tags, and much more.
+- `ux_tools`: gets readability stats for paragraphs of text (could
+          be from just `p` tags or a list of other tags e.g. `li`,
+          `div`, etc.).
 - `validator`: sends HTML or CSS code to the W3C Validator to check
           for errors.
 """
-__version__ = "0.4.3"
+__version__ = "0.5.0"
