9d8bf2956ecee9dd14742882c15cfdf6d65727f4
Chris Winikka <3827026+HundredVisionsGuy@users.noreply.github.com>
2023-09-27 17:24:11 -0700

    Dev (#8)
    * renaming files to avoid file-clerk conflicts and improving ux
    
    * fixing nav in docs
    
    * fixing nav in docs
    
    * updating version
    
    * adding docstring and type annotations
    
    * adding get_all_stylesheets_by_file function
    
    * updating to version 0.6.2

--- tests/test_css_tools.py
+++ tests/test_css_tools.py
@@ -119,6 +119,7 @@ h4 {
 path_to_gradients_project = "tests/test_files/"
 path_to_gradients_project += "projects/page_with_gradients_and_alpha/style.css"
 path_to_general_css = "tests/test_files/large_project/css/general.css"
+gallery_path = "tests/test_files/large_project/gallery.html"
 
 
 @pytest.fixture
@@ -588,6 +589,16 @@ def test_remove_external_imports(css_with_external_imports):
 
 def test_remove_two_external_imports(css_with_two_external_imports):
     assert "http" not in css_with_two_external_imports.text
+
+
+def test_get_all_stylesheets_by_file_for_4_sheets():
+    results = css_tools.get_all_stylesheets_by_file(gallery_path)
+    assert len(results) == 4
+
+
+def test_get_all_stylesheets_for_style_tag():
+    results = css_tools.get_all_stylesheets_by_file(gallery_path)
+    assert "styletag" in results[0].type
 
 
 def test_get_font_families_for_one(css_with_external_imports):
