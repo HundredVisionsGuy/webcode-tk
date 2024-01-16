a3a0783c57cbbbeb9eaacaa4f50871669e613040
Winikka <winikkc@hsd.k12.or.us>
2024-01-16 13:27:57 -0800

    test: adding tests to check for color changes to links and non-links

--- tests/test_cascade_tools.py
+++ tests/test_cascade_tools.py
@@ -17,5 +17,28 @@ def single_file_tree():
     return css_tree
 
 
+@pytest.fixture
+def single_file_link(single_file_tree):
+    p = single_file_tree.children[0].children[1]
+    a = p.children[0]
+    return a
+
+
 def test_css_tree_for_tree(single_file_tree):
     assert single_file_tree
+
+
+def test_single_link_for_color(single_file_link):
+    color = single_file_link.styles.get("color")
+    bg_color = single_file_link.styles.get("background-color")
+    assert color == "rgb(228, 234, 220)" and bg_color == "darkblue"
+
+
+def test_element_for_non_link():
+    h1_tag = cascade.Element("h1")
+    assert h1_tag
+
+
+def test_element_for_link_styles():
+    link = cascade.Element("a")
+    assert link.styles.get("color") == cascade.default_link_color
