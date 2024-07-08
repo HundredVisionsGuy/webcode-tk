b7d43e717a713888cc58e6f353180853fdbd96e1
Winikka <winikkc@hsd.k12.or.us>
2024-07-08 12:29:20 -0700

    adding font_tools and most function's tests

--- tests/test_font_tools.py
+++ tests/test_font_tools.py
@@ -7,6 +7,100 @@ sample_file_path = "tests/test_files/font-sizes.html"
 
 sample_file = css.get_all_stylesheets_by_file(sample_file_path)
 
+size_unit_test_data = [
+    ("14rem", "rem"),
+    ("x-small", "absolute_keyword"),
+    ("larger", "relative_keyword"),
+    ("80%", "percentage"),
+    ("0.8em", "em"),
+    ("12px", "pixels"),
+]
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    size_unit_test_data,
+    ids=[
+        "rems",
+        "absolute_keywords",
+        "relative_keywords",
+        "percentages",
+        "ems",
+        "pixels",
+    ],
+)
+def test_get_font_unit(value, expected):
+    results = fonts.get_font_unit(value)
+    assert results == expected
+
+
+value_unit_data = [
+    ("14rem", (14.0, "rem")),
+    ("x-small", ("x-small", "absolute_keyword")),
+    ("larger", ("larger", "relative_keyword")),
+    ("80%", (80.0, "%")),
+    ("0.8em", (0.8, "em")),
+    ("12px", (12, "px")),
+]
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    value_unit_data,
+    ids=["14rem", "x-small", "larger", "80%", "0.8em", "12px"],
+)
+def test_split_value_unit(value, expected):
+    results = fonts.split_value_unit(value)
+    assert results == expected
+
+
+# order: val, unit, parent_size, element, expected
+compute_font_size_data = [
+    ("initial", "initial", 20.0, "h2", 16.0),
+    (
+        110,
+        "%",
+        20.0,
+        "span",
+        22.0,
+    ),
+    (110, "percentage", 22.0, "span", 24.2),
+    (110, "percent", 24.2, "span", 26.6),
+    (1.2, "rem", 20.0, "p", 19.2),
+    (1.2, "em", 20.0, "p", 24.0),
+    ("x-small", "absolute_keyword", 20.0, "li", 10.0),
+    ("larger", "relative_keyword", 13.0, "li", 15.6),
+    ("smaller", "relative_keyword", 13.0, "li", 10.8),
+    ("unset", "unset", 20.0, "p", 20.0),
+    ("revert", "revert", 20.0, "p", 20.0),
+    ("revert", "revert", 20.0, "h3", 23.4),
+    ("inherit", "inherit", 20.0, "h3", 20.0),
+]
+
+
+@pytest.mark.parametrize(
+    "value,unit,parent_size,element,expected",
+    compute_font_size_data,
+    ids=[
+        "initial",
+        "%",
+        "percentage",
+        "percent",
+        "rem",
+        "em",
+        "absolute keyword",
+        "relative larger",
+        "relative smaller",
+        "unset",
+        "revert p",
+        "revert h3",
+        "inherit",
+    ],
+)
+def test_compute_font_size(value, unit, parent_size, element, expected):
+    results = fonts.compute_font_size(value, unit, parent_size, element)
+    assert results == expected
+
 
 @pytest.fixture
 def font_page():
@@ -20,16 +114,28 @@ def font_page_styles(font_page):
 
 def test_font_tools_for_num_keywords(font_page_styles):
     keywords = fonts.get_absolute_keyword_values(font_page_styles)
-    assert len(keywords) == 11
+    assert len(keywords) == 9
 
 
 def test_get_absolute_keyword_values_with_stylesheet(font_page):
-    expected = 11
+    expected = 9
     results = fonts.get_absolute_keyword_values(font_page)
     assert len(results) == expected
 
 
 def test_get_numeric_fontsize_values_for_num(font_page_styles):
-    expected = 2
+    expected = 5
     results = fonts.get_numeric_fontsize_values(font_page_styles)
     assert len(results) == expected
+
+
+def test_compute_keyword_size_for_xxxlarge():
+    expected = 48
+    results = fonts.compute_keyword_size("xxx-large")
+    assert results == expected
+
+
+def test_compute_rem_for_1_2():
+    expected = 19.2
+    results = fonts.compute_rem("1.2")
+    assert results == expected
--- webcode_tk/font_tools.py
+++ webcode_tk/font_tools.py
@@ -15,18 +15,28 @@ from typing import Union
 from webcode_tk.css_tools import Stylesheet
 
 absolute_keyword_regex = r"\b(?:xx-small|x-small|small|medium|large|x-large|"
-absolute_keyword_regex += r"xx-large|xxx-large|smaller|larger)\b"
+absolute_keyword_regex += r"xx-large|xxx-large)\b"
+relative_keyword_regex = r"\b(?:smaller|larger)\b"
 numeric_value_regex = r"(\d+(\.\d+)?)(px|em|rem|pt|%)"
+relative_keyword = re.compile(relative_keyword_regex)
+HEADING_SIZES = {
+    "h1": "2.0em",
+    "h2": "1.5em",
+    "h3": "1.17em",
+    "h4": "1.0em",
+    "h5": "0.83em",
+    "h6": "0.67em",
+}
 
 KEYWORD_SIZE_MAP = {
-    "xx-small": "9px",
-    "x-small": "10px",
-    "small": "13px",
-    "medium": "16px",
-    "large": "18px",
-    "x-large": "24px",
-    "xx-large": "32px",
-    "xxx-large": "48px",
+    "xx-small": 9,
+    "x-small": 10,
+    "small": 13,
+    "medium": 16,
+    "large": 18,
+    "x-large": 24,
+    "xx-large": 32,
+    "xxx-large": 48,
 }
 
 
@@ -76,8 +86,172 @@ def get_numeric_fontsize_values(css_code: Union[str, Stylesheet]) -> list:
     else:
         code = css_code
     matches = re.findall(numeric_value_regex, code)
-    full_matches = ["".join(match) for match in matches]
+    full_matches = []
+    for match in matches:
+        full_matches.append(match[0] + match[-1])
     return full_matches
+
+
+def get_font_unit(value: str) -> str:
+    """returns unit being use when setting font-size"""
+    unit = ""
+    # Check for relative keywords
+    if value in KEYWORD_SIZE_MAP.keys():
+        unit = "absolute_keyword"
+    elif relative_keyword.search(value):
+        unit = "relative_keyword"
+    elif "rem" in value:
+        unit = "rem"
+    elif "em" in value:
+        unit = "em"
+    elif "px" in value:
+        unit = "pixels"
+    elif "%" in value:
+        unit = "percentage"
+    elif value == 0:
+        unit = "zero"
+    else:
+        unit = value
+    return unit
+
+
+def split_value_unit(value: str) -> tuple:
+    """returns the numeric value and unit of a size value
+
+    First, check for absolute and relative keywords, then split at
+    number
+
+    Args:
+        value: the CSS value (from a font-size declaration).
+
+    Returns:
+        value_unit: a tuple of the value and the unit.
+    """
+    value_unit = ["", ""]
+    if value in KEYWORD_SIZE_MAP.keys():
+        value_unit = (value, "absolute_keyword")
+    elif relative_keyword.search(value):
+        value_unit = (value, "relative_keyword")
+    elif value in ["0", "0.0"]:
+        value_unit = (value, "zero")
+    else:
+        # do a regex split
+        num = re.findall(r"[+-]?(\d*\.\d+|\d+)", value)[0]
+        unit = re.findall(r"%|rem|em|px", value)[0]
+        value_unit = (float(num), unit)
+    return tuple(value_unit)
+
+
+def compute_rem(value: str) -> float:
+    """returns computed value in pixels
+
+    Convert rems to pixels by multiplying the rem value to
+    pixels (rem * 16)
+
+    Args:
+        value: the value only of a rem.
+
+    Returns:
+        pixel: the computed size in pixels
+    """
+    pixels = float(value) * 16
+    return pixels
+
+
+def compute_keyword_size(value: str) -> int:
+    """return computed value in pixels
+
+    Extracts the keyword and uses it to get the value. Uses
+    the absolute_keyword_regex.
+
+    Args:
+        value: the value from the font-size declaration.
+
+    Returns:
+        pixels: the font-size of the keyword in pixels.
+    """
+    pixels = 0
+    keyword = re.findall(absolute_keyword_regex, value)[0]
+    pixels = KEYWORD_SIZE_MAP.get(keyword)
+    return pixels
+
+
+def is_large_text(size: float, is_bold: bool) -> bool:
+    """returns whether the text is considered large or not
+
+    This is based on the WebAIM color contrast analyzer, which
+    states that text is large if it is 18.66 and bold or larger
+    or 24px or larger.
+
+    Args:
+        size: the computed font-size.
+        is_bold: whether the text is bold or not.
+
+    Returns:
+        is_large: whether the text is considered large or not.
+    """
+    is_large = False
+    return is_large
+
+
+def compute_font_size(
+    value: Union[str, int, float],
+    unit: str,
+    parent_size: Union[int, float] = 16.0,
+    element_name: str = "",
+) -> float:
+    """Computes font size based on value, unit, and possibly parent size.
+
+    This is for handling any font-size declaration value. Some sizes are
+    fixed, and others are relative based on relative keyword, root size,
+    or parent's computed size.
+
+    The default size for parent_size is the root size of a font (by default,
+    it is 16px).
+
+    Args:
+        value: numeric or keyword value (could be a string, int, or float).
+        unit: the unit of measurement as a string.
+        parent_size: if applicable, the computed font size of the parent.
+        element_name: optionally, you may set this, but it's only necessary
+            if your element is an h1 - h6 tag.
+
+    Returns:
+        computed_size: the computed size in pixels.
+
+    Raises:
+        TypeError: if the unit is not recognized (may be a true error, or
+            may be using a new font-size)
+    """
+    computed_size = 16.0
+    if unit == "absolute_keyword":
+        computed_size = compute_keyword_size(value)
+    elif unit == "relative_keyword":
+        if value == "larger":
+            computed_size = parent_size * 1.2
+        elif value == "smaller":
+            computed_size = parent_size * 0.833
+    elif unit == "rem":
+        computed_size = compute_rem(value)
+    elif unit in ("percent", "percentage", "%"):
+        percentage = value * 0.01
+        computed_size = parent_size * percentage
+    elif unit == "em":
+        computed_size = parent_size * value
+    elif unit in ("unset", "inherit"):
+        computed_size = parent_size
+    elif unit in "revert":
+        if element_name in HEADING_SIZES.keys():
+            ems = HEADING_SIZES.get(element_name)
+            multiplier = ems.split("e")[0]
+            computed_size = parent_size * float(multiplier)
+        else:
+            computed_size = parent_size
+    elif unit == "initial":
+        computed_size = 16.0
+    else:
+        raise TypeError("I'm afraid I can't compute that, Dave")
+    return round(computed_size, 1)
 
 
 if __name__ == "__main__":
